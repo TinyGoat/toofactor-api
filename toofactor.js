d234b320613cb6d0705b669d182c5e9b8d4e03ae
Roger Gregory <rtgregory@gmail.com>
2012-04-08 10:34:01 -0400

    Break out tokenize function from lookup; considering using Express as the underlying framework.

--- toofactor.js
+++ toofactor.js
@@ -1,7 +1,9 @@
-// Hey, ho, let's go
+// Gadget node-router
 //
-require('util');
 var server = require('./lib/node-router').getServer();
+
+// It rubs the Redis on it's skin
+//
 var redis = require('./node_modules/redis'),
     client = redis.createClient();
     client.on("error", function (err) {
@@ -10,30 +12,37 @@ var redis = require('./node_modules/redis'),
 
 // Generate hex token
 //
+function tokenize() {
+    var output = ""
+    var token = 0
+    for (i=0; i<=7; i=i+1) {
+        token = Math.floor(Math.random()*16).toString(16)
+        output = output + " " + token
+    }
+    return output
+}
+
+// Customer?
+//
 function customer_lookup(match) {
     if (client.get(match)) {
-        var output = ""
-        var token = 0
-        for (i=0; i<=7; i=i+1) {
-            token = Math.floor(Math.random()*16).toString(16)
-            output = output + " " + token
-        }
-        return output
-    } else {
+        return tokenize();
+   } else {
         return "";
     }
 }
 
-// We don't serve anything via root
+// Hey, ho, let's go!
 //
 server.get("/", function (req, res, match) {
   return ""; 
 });
 
-// Stage API access for future proofing
+// Pull URI for matching
 //
 server.get(new RegExp("^/(.*)$"), function hello(req, res, match) {
 	return customer_lookup(match);
+	 res.end();
 });
 
 // Go, go gadget Node
