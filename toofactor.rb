5877d9d871207c9d4d91f9dc75239d94646705cf
Roger Gregory <rtgregory@gmail.com>
2012-04-12 14:39:09 -0400

    Create client url's automatically

--- toofactor.rb
+++ toofactor.rb
@@ -8,10 +8,12 @@ require 'sinatra'
   set :static_cache_control, [:private, :max_age => 60]
   set :public_folder, 'public'
 
+require 'sinatra/cookies'
+
 require 'json'
 require 'builder'
 require 'haml'
-require 'sinatra/cookies'
+require 'digest/sha1'
 
 configure :production do
 
@@ -25,12 +27,24 @@ configure :production do
     etag sha1
     last_modified date
   end
+
+  # Find Godot
+  #
+  not_found do
+    haml :notfound
+  end
+  
+  error do
+    'Sorry there was a nasty error - ' + env['sinatra.error'].name
+  end
+
 end
 
 # Ruby dudes are all about class baby
 # 
 class TooFactor < Sinatra::Application
  
+  $base_url = "http://toofactor.com/client/"
   redis_host = "127.0.0.1"
 
   # Everything is a hash
@@ -53,9 +67,14 @@ class TooFactor < Sinatra::Application
   def tokenize(min, max, token)
     token = ""
     (min..max).each do 
-    token = token + gen_hex
+      token = token + gen_hex
     end
     return token
+  end
+
+  def record_client_token(client_sha, token)
+    $redis.set(client_sha, token)
+    $redis.expire(client_sha, 90)
   end
 
   def customer_match(match)
@@ -63,14 +82,23 @@ class TooFactor < Sinatra::Application
     return tokenize(0, 7, customer)
   end
 
+  def create_client_hash(cmatch)
+    client_sha = Digest::SHA2.new << cmatch
+    client_url = $base_url + client_sha.to_s
+    record_client_token(client_sha, cmatch)
+    return client_url
+  end
+
   def json_token(cmatch, tstamp)
+    client_url = create_client_hash(cmatch)
     content_type :json
-    { :auth => cmatch, :timestamp => tstamp }.to_json
+    { :auth => cmatch, :timestamp => tstamp, :client_url => client_url }.to_json
   end
 
   def xml_token(cmatch, tstamp)
+    client_url = create_client_hash(cmatch)
     xml = Builder::XmlMarkup.new
-    xml.token { |b| b.auth(cmatch); b.timestamp(tstamp) }
+    xml.token { |b| b.auth(cmatch); b.timestamp(tstamp); b.client_url(client_url) }
   end
 
   # Move along son
@@ -101,7 +129,7 @@ class TooFactor < Sinatra::Application
   get %r{/api/([\w]+)/([\w]+)} do |match,type|
     confirm = "#{match}"
     confirm.freeze
-    #begin 
+    # begin 
       if (customer?(confirm))
         tstamp = Time.now.to_f
         cookies[:TooFactor] = tstamp
@@ -117,19 +145,21 @@ class TooFactor < Sinatra::Application
       else
         haml :nomatch
       end
-    #rescue
+    # rescue
     #  haml :eek
    # end
   end
 
-  # Finding Godot
+  # Client access
   #
-  not_found do
-      haml :notfound
+  get %r{/client/([\w]+)/$} do |match|
+    client = "#{match}"
+    if (client_token_exist?(client))
+      client_token(client)
+    else
+      haml :client_expired
+    end
   end
-
-  error do
-      'Sorry there was a nasty error - ' + env['sinatra.error'].name
-  end
+    
 end
 
