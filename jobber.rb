100b675c0c02f0fa5711ce78793c78f408f80761
Roger Gregory <rtgregory@gmail.com>
2012-05-10 16:33:30 -0400

    Skel out ActiveRecord

--- jobber.rb
+++ jobber.rb
@@ -1,6 +1,9 @@
 require 'redis'
 require 'uri'
+require 'activerecord'
 
+# Redis
+#
 ENV["REDISTOGO_URL"] = 'redis://redistogo:809165c597aee3f873f3a0776ba03cac@gar.redistogo.com:9163'
 uri = URI.parse(ENV["REDISTOGO_URL"])
 
@@ -13,11 +16,16 @@ rescue
   retry
 end
 
+class Customer < ActiveRecord::Base
+end
+
+ActiveRecord::Base.establish_connection(
+  :adapter => 'sqlite3',
+  :dbfile =>  'example.sqlite3.db'
+)
+
 # Dev
 # @redis_log  = Redis.new(:host => "127.0.0.1", :port => 6379, :timeout => 0)     
-
-
-
 
 # Loop
 #
