fbbb4e4000df4a497846ac3f069efc4b672970c4
Roger Gregory <rtgregory@gmail.com>
2012-05-10 13:38:09 -0400

    Jobber now understands shared Redis instance; no longer craps it's pants if it cannot connect to Redis.

--- jobber.rb
+++ jobber.rb
@@ -1,29 +1,35 @@
 require 'redis'
-require 'json'
+require 'uri'
 
-$redis_log  = Redis.new(:host => "127.0.0.1", :port => 6379, :timeout => 0)     
+ENV["REDISTOGO_URL"] = 'redis://redistogo:809165c597aee3f873f3a0776ba03cac@gar.redistogo.com:9163'
+uri = URI.parse(ENV["REDISTOGO_URL"])
 
-# 
-def push_to_sql
-  
+begin
+  @redis_log = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+  @redis_log.inspect
+rescue Exception => e
+  p "Cannot access Redis instance.."
+  sleep 10
+  retry
 end
 
-def notify_user
-  
-end
+# Dev
+# @redis_log  = Redis.new(:host => "127.0.0.1", :port => 6379, :timeout => 0)     
 
-def expire_user
-  
-end
 
 
 
 # Loop
 #
-$redis_log.subscribe('LOG') do |on|
-  on.message do |channel, data|
-    customer_api, client_address, type = data.split(":")
-    puts "#{customer_api} - #{client_address} #{type}"
+begin
+  @redis_log.subscribe('LOG') do |on|
+    on.message do |channel, data|
+      customer_api, client_address, type = data.split(":")
+      puts "#{customer_api} - #{client_address} #{type}"
+    end
   end
+rescue Exception => e
+  p "Error accessing Redis channel .. retrying"
+  sleep 2
+  retry
 end
-
